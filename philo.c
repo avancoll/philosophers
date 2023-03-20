f2fdd1147f05598f7306897ff9445fec58b3f60b
Antoine Vancollie <avancoll@shi-r4-p4.s19.be>
2023-03-20 17:22:49 +0100

    small changes

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: avancoll <avancoll@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:29:05 by avancoll          #+#    #+#             */
-/*   Updated: 2023/03/20 15:11:07 by avancoll         ###   ########.fr       */
+/*   Updated: 2023/03/20 17:20:30 by avancoll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,11 @@ int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
+	struct timeval start, end;
 
 	mail = 0;
 	pthread_mutex_init(&mutex, NULL);
+	gettimeofday(&start, NULL);
 	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
 		return (1);
 	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
@@ -46,5 +48,9 @@ int	main(int argc, char **argv)
 		return (1);
 	pthread_mutex_destroy(&mutex);
 	printf("%d\n", mail);
+	gettimeofday(&end, NULL);
+	printf("Time taken: %ld micro seconds\n",
+	((end.tv_sec * 1000000 + end.tv_usec) -
+	(start.tv_sec * 1000000 + start.tv_usec)));
 	return (0);
 }
