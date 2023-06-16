fd3ed41bd9f9130b0ae598bf9c2e9568d2834d8d
Antoine Vancollie <avancoll@fu-c3-p3.s19.be>
2023-06-16 17:16:26 +0200

    patched when 0 philo and time_to_die when 1 philo

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: avancoll <avancoll@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 17:02:20 by avancoll          #+#    #+#             */
-/*   Updated: 2023/05/25 16:45:47 by avancoll         ###   ########.fr       */
+/*   Updated: 2023/06/16 17:14:00 by avancoll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,17 @@ int	main(int argc, char **argv)
 		return (ft_error(NULL, 0));
 	if (init_data(&table, argv))
 		return (ft_error(NULL, 1));
+	if (table.nb_philo == 0)
+		return (0);
+	if (init_philo(&table))
+		return (ft_error(&table, 2));
 	if (table.nb_philo == 1)
 	{
 		printf("[%ld] 1 has taken a fork.\n", get_time(table.start_time));
-		printf("[%ld] 1 died.\n", get_time(table.start_time));
-		return (0);
+		ft_usleep(table.philo, table.time_to_die);
+		printf("[%d] 1 died.\n", table.time_to_die);
+		return (free_all(&table));
 	}
-	if (init_philo(&table))
-		return (ft_error(&table, 2));
 	if (init_thread(&table))
 		return (ft_error(&table, 3));
 	i = -1;
